0441e7cc061826d371dfedd9e5442493c347d291
gamrom <gamrom01@naver.com>
2023-02-07 23:04:33 +0900

    tailwind setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import { css } from '@emotion/react'
+
 export default function Home() {
   return (
-    <>
+    <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
      hi!
-    </>
+    </div>
   )
 }
