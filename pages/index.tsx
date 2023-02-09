b8a5d4b1fb396233ce473d3be3f2e5380882c8d5
gamrom <gamrom01@naver.com>
2023-02-09 20:22:03 +0900

    바벨 설정 프리티어 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import { css } from '@emotion/react'
+import { css } from "@emotion/react";
 
 export default function Home() {
   return (
     <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-     hi!
+      hi!
     </div>
-  )
+  );
 }
