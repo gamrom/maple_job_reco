ff4685f527e5b1aea135c96e299aa89c82e268d4
gamrom <gamrom01@naver.com>
2023-04-13 10:04:08 +0900

    저장

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css"
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { css } from "@emotion/react";
 export default function Home() {
   return (
     <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-      hi!
+      <span className="text-xl">메이플 직업 추천</span>
     </div>
   );
 }
